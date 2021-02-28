bb40adabdddb1ade734199350b8af48122ed18f2
Vicky2500 <78389671+Vicky2500@users.noreply.github.com>
2021-03-01 05:22:44 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -93,18 +93,18 @@ async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("**𖣘**")
-    await pong.edit("**𖣘𖣘**")
-    await pong.edit("**𖣘𖣘𖣘**")
-    await pong.edit("**✦҈͜͡➳ PONG!**")
+    await pong.edit("**👌**")
+    await pong.edit("**👉👌**")
+    await pong.edit("**👉👌💦**")
+    await pong.edit("**✦҈͜͡➳ Pinging!!!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**♛ Lord Ping ♛**\n"
-                    f"❃ **Ping:** "
+    await pong.edit(f"**⚙️ Pinging!!! ⚙️**\n"
+                    f"⚙️ **Ping:** "
                     f"`%sms` \n"
-                    f"❃ **Uptime:** "
+                    f"🕛 **Uptime:** "
                     f"`{uptime}` \n"
-                    f"__**✦҈͜͡➳ My Lord:**__ `{ALIVE_NAME}`" % (duration))
+                    f"__**👑 KING:**__ `{ALIVE_NAME}`" % (duration))
 
 
 @register(outgoing=True, pattern="^.speed$")
